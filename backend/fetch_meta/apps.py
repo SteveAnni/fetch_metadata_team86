bc31ea9b26cec9146b8a8d51b0ed2be866acb2f9
Animashaun olasunkanmi <animashaunstephen@gmail.com>
2022-08-07 06:20:32 +0100

    header and Footer

---
+++ backend/fetch_meta/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FetchMetaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fetch_meta'
--- backend/fetch_metadata/settings.py
+++ backend/fetch_metadata/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'rest_framework.authtoken',
-    'metafetch_api',
+    'metafetch_api'
+    'fetch_meta',
 
 ]
 
